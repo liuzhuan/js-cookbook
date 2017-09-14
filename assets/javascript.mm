9594799883de93586fec8c838be3f3666a5157ab
liuz <liuzhuankyzy@gmail.com>
2017-09-14 23:41:16 +0800

    add javascript mind map in pure text format

---
+++ assets/javascript.mm
@@ -0,0 +1,73 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="javascript" FOLDED="false" ID="ID_1381147837" CREATED="1505401860362" MODIFIED="1505402878742"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#000000ff,#ff0033ff,#009933ff,#3333ffff,#ff6600ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt" TEXT_SHORTENED="true">
+<font SIZE="24"/>
+<richcontent TYPE="DETAILS" LOCALIZED_HTML="styles_background_html"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0.0 pt" TEXT_ALIGN="CENTER" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+<font NAME="Arial" SIZE="9" BOLD="true" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="11" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font SIZE="9" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT">
+<font BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+<edge COLOR="#0000cc"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" UNIFORM_SHAPE="true" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+<font SIZE="24" ITALIC="true"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font SIZE="24"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_COLUMNS"/>
+<node TEXT="&#x68c0;&#x6d4b;&#x7c7b;&#x578b;" FOLDED="true" POSITION="right" ID="ID_1166944756" CREATED="1505402897821" MODIFIED="1505402902281">
+<node TEXT="&#x57fa;&#x672c;&#x6570;&#x636e;&#x7c7b;&#x578b;" ID="ID_205746763" CREATED="1505402904057" MODIFIED="1505402916900">
+<node TEXT="typeof" ID="ID_1216968364" CREATED="1505402919359" MODIFIED="1505402921429">
+<node TEXT="number" ID="ID_1880953069" CREATED="1505402977934" MODIFIED="1505402981226"/>
+<node TEXT="string" ID="ID_1900561585" CREATED="1505402988435" MODIFIED="1505402989901"/>
+<node TEXT="object" ID="ID_1447929205" CREATED="1505403010139" MODIFIED="1505403011925"/>
+<node TEXT="boolean" ID="ID_753838451" CREATED="1505403024947" MODIFIED="1505403026521"/>
+<node TEXT="undefined" ID="ID_1235708669" CREATED="1505403035068" MODIFIED="1505403036820"/>
+<node TEXT="function" ID="ID_150423019" CREATED="1505403055802" MODIFIED="1505403058232"/>
+</node>
+</node>
+<node TEXT="&#x5f15;&#x7528;&#x7c7b;&#x578b;" ID="ID_841956434" CREATED="1505402949911" MODIFIED="1505402958356">
+<node TEXT="instanceof" ID="ID_688979748" CREATED="1505402959379" MODIFIED="1505402963491"/>
+</node>
+</node>
+<node TEXT="&#x6267;&#x884c;&#x73af;&#x5883;&#xa;execution context" POSITION="left" ID="ID_1996347454" CREATED="1505403226973" MODIFIED="1505403264992">
+<node TEXT="&#x53d8;&#x91cf;&#x5bf9;&#x8c61;&#xa;variable object" ID="ID_1036512980" CREATED="1505403309677" MODIFIED="1505403330520">
+<node TEXT="&#x53d8;&#x91cf;" ID="ID_864134459" CREATED="1505403343072" MODIFIED="1505403349612"/>
+<node TEXT="&#x51fd;&#x6570;" ID="ID_817319508" CREATED="1505403350100" MODIFIED="1505403354985"/>
+<node TEXT="&#x4f5c;&#x7528;&#x57df;&#x94fe;&#xa;scope chain" ID="ID_901942017" CREATED="1505403471372" MODIFIED="1505403487667"/>
+</node>
+</node>
+<node TEXT="&#x4f5c;&#x7528;&#x57df;" POSITION="left" ID="ID_827704753" CREATED="1505403238269" MODIFIED="1505403255958"/>
+</node>
+</map>
