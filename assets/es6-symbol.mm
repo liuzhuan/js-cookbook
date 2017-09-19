71ff9d8b41cdf2ee647e07264c17095dfcb1e985
liuz <liuzhuan@qingsongchou.com>
2017-09-19 11:07:31 +0800

    add es6 symbol mind map

---
+++ assets/es6-symbol.mm
@@ -0,0 +1,77 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Symbol" FOLDED="false" ID="ID_1976203834" CREATED="1505789972405" MODIFIED="1505789976998" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="&#x65e0;&#x9700; new" FOLDED="true" POSITION="right" ID="ID_253507621" CREATED="1505789977639" MODIFIED="1505789990554">
+<node TEXT="let s = Symbol()&#xa;typeof s // &quot;symbol&quot;" ID="ID_1868748315" CREATED="1505789992992" MODIFIED="1505790010994"/>
+<node TEXT="var s1 = Symbol(&apos;foo&apos;)&#xa;s1 // Symbol(foo)&#xa;s1.toString() // &quot;Symbol(foo)&quot;" ID="ID_1588207059" CREATED="1505790147580" MODIFIED="1505790181792"/>
+</node>
+<node TEXT="&#x4e0d;&#x80fd;&#x4e0e;&#x5176;&#x4ed6;&#x7c7b;&#x578b;&#x7684;&#x503c;&#x8fd0;&#x7b97;" POSITION="right" ID="ID_238264726" CREATED="1505790236334" MODIFIED="1505790250473">
+<node TEXT="var sym = Symbol(&apos;My symbol&apos;)&#xa;&quot;your symbol is &quot; + sym // TypeError" ID="ID_1084366580" CREATED="1505790251839" MODIFIED="1505790280105"/>
+</node>
+<node TEXT="&#x76ee;&#x7684;" POSITION="left" ID="ID_1636442226" CREATED="1505790042713" MODIFIED="1505790048171">
+<node TEXT="&#x4fdd;&#x8bc1;&#x5c5e;&#x6027;&#x540d;&#x72ec;&#x4e00;&#x65e0;&#x4e8c;" ID="ID_1315286908" CREATED="1505790048658" MODIFIED="1505790057596"/>
+</node>
+</node>
+</map>
